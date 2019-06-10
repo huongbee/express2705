83e76c749a3d3f55260b855ea2119bd364c3edeb
Huong <huongnguyenak96@gmail.com>
2019-06-10 20:21:24 +0700

    Calculatetor

--- index.js
+++ index.js
@@ -15,5 +15,28 @@ app.get('/detail/:alias',(req,res)=>{
     res.send({ alias })
 })
 
+app.get('/:pheptinh/:a/:b',(req, res)=>{
+    const { pheptinh, a, b } = req.params
+})
+
 const port = 3000;
 app.listen(port, ()=>console.log(`Server start on port ${port}`))
+
+class Calculatetor{
+    constructor(pheptinh, a, b){
+        this.pheptinh = pheptinh;
+        this.a = a;
+        this.b = b;
+    }
+    get pt(){
+        if(this.pheptinh==='cong') return '+';
+        if(this.pheptinh==='tru') return '-';
+        if(this.pheptinh==='nhan' ) return '*';
+        if(this.pheptinh==='chia') return '/';
+        if(this.pheptinh==='chiadu') return '%';
+    }
+    get result(){
+        let r = this.a+this.pt+this.b;  // r=2+3
+        return eval(r);
+    }
+}
